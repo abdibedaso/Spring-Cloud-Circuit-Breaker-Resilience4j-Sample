1a0ae9e3d9e9ba8d46f8318388cf014266104638
Abdi T Bedaso <atbedaso@gmail.com>
2021-10-07 16:41:16 -0400

    create Service one app

---
+++ ServiceOne/src/test/java/com/geekcolasb/development/serviceone/ServiceOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geekcolasb.development.serviceone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceOneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
